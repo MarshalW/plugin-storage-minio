f09b2fb58241da309b75f367802b4c932816f7e9
Marshal Wu <marshal.wu@qq.com>
2026-01-10 22:20:36 +0800

    增加一个演示用的下载 action

--- packages/plugins/@marshal/plugin-storage-minio/src/server/plugin.ts
+++ packages/plugins/@marshal/plugin-storage-minio/src/server/plugin.ts
@@ -10,6 +10,36 @@ export class PluginStorageMinioServer extends Plugin {
   async load() {
     const plugin = this.app.pm.get(PluginFileManagerServer);
     plugin.registerStorageType('minio-storage', StorageTypeMinio);
+
+    this.app.resourceManager.define({
+      name: 'dl',
+      actions: {
+        myFile: async (ctx, next) => {
+          const { attachmentId } = ctx.action.params;
+
+          const filePlugin = this.app.pm.get(PluginFileManagerServer) as PluginFileManagerServer;
+          const AttachmentRepo = this.db.getRepository('attachments');
+          const record = await AttachmentRepo.findOne({
+            filterByTk: attachmentId,
+            appends: ['storage'],
+          });
+
+          if (!record) {
+            return ctx.throw(404, '附件不存在');
+          }
+
+          const { stream, contentType } = await filePlugin.getFileStream(record);
+
+          ctx.type = contentType || 'application/octet-stream';
+          ctx.set('Content-Disposition', `inline; filename*=UTF-8''${encodeURIComponent(record.filename)}`);
+          ctx.body = stream;
+
+          await next();
+        },
+      },
+    });
+
+    this.app.acl.allow('dl', '*', 'public');
   }
 
   async install() { }
